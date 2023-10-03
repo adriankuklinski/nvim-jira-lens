3865c526e052169d03fb20d39c11da5d82c8a98a
Adrian Kuklinski <adriankuklinski@pm.me>
2023-10-03 18:00:51 -0400

    fix: Update module names

--- lua/issue-me-daddy/init.lua
+++ lua/issue-me-daddy/init.lua
@@ -5,10 +5,10 @@ M.setup = function()
     local password = vim.fn.inputsecret("Jira Password: ")
     local base_url = vim.fn.input("Jira Base URL: ")
 
-    require'nvim-jira.config'.set_credentials(username, password, base_url)
+    require'issue-me-daddy.config'.set_credentials(username, password, base_url)
 end
 
-vim.cmd [[ command! JiraSetup lua require'nvim-jira'.setup() ]]
+vim.cmd [[ command! IssueMeSetup lua require'issue-me-daddy'.setup() ]]
 
 return M
 
