6a187f95d98c88c31d8264ce49ab1c8271ea0d53
Adrian Kuklinski <adriankuklinski@pm.me>
2023-10-05 14:40:42 -0400

    feat: Update telescope show_issues method to trigger telescope with jira data

--- lua/issue-me-daddy/telescope.lua
+++ lua/issue-me-daddy/telescope.lua
@@ -1,18 +1,18 @@
 local M = {}
-local api = require'issue-me-daddy.api'
-local telescope = require'telescope'
+
+local telescope = require('telescope')
+local api = require('issue-me-daddy.api')
 
 M.show_issues = function(config)
-    -- Fetch issues using the API
     local issues = api.get_my_issues(config)
 
-    -- Convert the issues into a format suitable for Telescope
     local issue_entries = {}
     for _, issue in ipairs(issues) do
         table.insert(issue_entries, {
-            value = issue.id,
-            display = issue.summary,
-            ordinal = issue.summary,
+            value = issue.key,
+            display = issue.key .. ": " .. issue.summary,
+            ordinal = issue.summary .. " " .. issue.description,
+            description = issue.description
         })
     end
 
@@ -23,7 +23,13 @@ M.show_issues = function(config)
             results = issue_entries,
         }),
         sorter = telescope.config.values.generic_sorter({}),
+        previewer = telescope.previewers.new_buffer_previewer({
+            define_preview = function(self, entry, status)
+                return entry.description
+            end
+        }),
     }):find()
 end
 
 return M
+
