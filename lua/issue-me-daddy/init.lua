fb44eb238fb4f7d21a836ee4f6c5c678d5a24807
Adrian Kuklinski <adriankuklinski@pm.me>
2023-10-07 21:48:34 -0400

    refactor: Simplify setup method logic and error handling

--- lua/issue-me-daddy/init.lua
+++ lua/issue-me-daddy/init.lua
@@ -1,33 +1,19 @@
 local M = {}
+
 M.config = {}
 
--- Setup function to initialize and configure the plugin
-M.setup = function(opts)
-    -- Use provided options or default to an empty table
-    opts = opts or {}
-
-    -- Default configuration values
-    local default_opts = {
+M.setup = function(user_config)
+    local default_config = {
         username = "",
         password = "",
         base_url = "",
     }
+    M.config = vim.tbl_extend('force', default_config, user_config)
 
-    -- Overwrite defaults with user provided options where available
-    for k, v in pairs(default_opts) do
-        if opts[k] == nil then
-            opts[k] = v
-        end
+    -- Validate required options
+    for _, opt in ipairs({"base_url", "username", "password"}) do
+        assert(M.config[opt] and M.config[opt] ~= "", string.format("You must provide a valid %s for issue-me-daddy!", opt))
     end
-
-    local required_opts = {"base_url", "username", "password"}
-    for _, opt in ipairs(required_opts) do
-        if not opts[opt] or opts[opt] == "" then
-            error(string.format("You must provide a valid %s for issue-me-daddy!", opt))
-        end
-    end
-
-    M.config = opts
 end
 
 return M
